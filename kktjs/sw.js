b665327954f086e2d636f5d9640c92290a5fe2c4
sumire-kkt <sumire.kkt@gmail.com>
2019-05-21 22:52:54 +0900

    [kktjs] 1.4.7 アンロックしたときの更新処理と、CacheAPIの処理を新しくしたよ

--- kktjs/sw.js
+++ kktjs/sw.js
@@ -1,21 +1,27 @@
 // const url = 'http://localhost:8080';
-const url = 'https://sumire.work';
+// const url = 'https://sumire.work';
+const url = location.origin;
 
-const key = 'v1.4.6_75';
+const key = "v1.4.7_413";
+const subkey = "?v=0512";
+console.log("sw: set cache "+key);
 
 const cache_keys = [
   key
 ];
 const file = [
   url + '/kktjs/',
-  url + '/kktjs/css/style.css',
-  url + '/kktjs/js/main_readable.js',
+  url + '/kktjs/sw.js',
+  url + '/kktjs/css/style.css'+subkey,
+  url + '/kktjs/js/main.js'+subkey,
+  url + '/kktjs/js/main_readable.js'+subkey,
   url + '/kktjs/css/font-awesome.min.css',
   url + '/kktjs/sounds/boop.mp3',
   url + '/kktjs/fonts/roboto.ttf',
   url + '/kktjs/fonts/fontawesome-webfont.woff',
   url + '/kktjs/fonts/fontawesome-webfont.woff2',
   url + '/kktjs/img/sheet_64_indexed_128.png',
+  url + '/kktjs/img/missing_header.png',
   url + '/kktjs/img/missing_icon.png',
   url + '/kktjs/img/favicon.ico',
   url + '/kktjs/img/touch/apple-touch-icon.png',
@@ -32,13 +38,12 @@ const file = [
   url + '/kktjs/js/lodash.min.js',
   url + '/kktjs/js/addtohomescreen.min.js',
   url + '/kktjs/js/vue.min.js',
-  url + '/kktjs/js/main.js',
   url + '/kktjs/js/picker.js',
   url + '/kktjs/js/manifest.json'
 ];
 
 self.addEventListener('install', event => {
-  // console.log("install");
+  // console.log("sw: install");
   self.skipWaiting();
   event.waitUntil(
     caches.open(key).then(cache => {
@@ -54,7 +59,7 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  // console.log("activate");
+  // console.log("sw: activate");
   self.clients.claim();
   event.waitUntil(
     caches.keys().then(keys => {
@@ -69,13 +74,36 @@ self.addEventListener('activate', event => {
   );
 });
 
-self.addEventListener('fetch', event => {
-  if(event.request.url == 'https://kirakiratter.com/api/v1/media'){
-    return;
-  };
+self.addEventListener('fetch', (event) => {
+  // if(event.request.url == 'https://kirakiratter.com/api/v1/media'){
+  //   // return;
+  // };
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.open(key).then((cache) => {
+      return cache.match(event.request).then((response) => {
+        // if(response && response.url.length != 0){
+        //   console.log("sw cache: "+response.url);
+        // }
+        return response || fetch(event.request).then((response) => {
+          if(/^https:\/\/cdn.jsdelivr.net\/|https:\/\/files.kirakiratter.com\/accounts\/avatars\//.test(event.request.url)){
+            // console.log("sw cache add: "+event.request.url);
+            cache.put(event.request, response.clone());
+          // }else{
+          //   console.log("sw fetch: "+event.request.url);
+          }
+          return response;
+        });
+      });
     })
   );
+});
+
+self.addEventListener('message', (event) => {
+  if(event.data == "check"){
+    console.log("sw: update check.");
+    self.registration.update();
+  // }else if(event.data == "force"){
+  //   console.log("sw: kktjs update now");
+  //   self.skipWaiting();
+  }
 });
